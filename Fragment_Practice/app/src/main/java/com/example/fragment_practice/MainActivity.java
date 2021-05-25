474c88bfc109584a8974bbe58bd73cdbd618651c
Maryam004 <mcsf19a039@pucit.edu.pk>
2021-05-24 22:24:07 -0700

    New Updation

--- Fragment_Practice/app/src/main/java/com/example/fragment_practice/MainActivity.java
+++ Fragment_Practice/app/src/main/java/com/example/fragment_practice/MainActivity.java
@@ -9,21 +9,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_SignIn,btn_SignUp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_SignIn=findViewById(R.id.signIn);
-        btn_SignUp=findViewById(R.id.signUp);
-        btn_SignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             FragmentManager fm=getSupportFragmentManager();
-             fm.beginTransaction().replace(R.id.holder,new Sign_In_Fragment());
-            }
-        });
     }
 }

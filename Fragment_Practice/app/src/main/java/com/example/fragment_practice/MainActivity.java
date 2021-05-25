18511737e3deda291f8e10e644379b0c946a2e96
Maryam004 <mcsf19a039@pucit.edu.pk>
2021-05-24 23:07:29 -0700

    New Updation

--- Fragment_Practice/app/src/main/java/com/example/fragment_practice/MainActivity.java
+++ Fragment_Practice/app/src/main/java/com/example/fragment_practice/MainActivity.java
@@ -31,7 +31,7 @@ Button b1,b2;
             public void onClick(View v) {
                 FragmentManager fm=getSupportFragmentManager();
                 FragmentTransaction fr=fm.beginTransaction();
-                //fr.add(R.id.holder,new Sign_Up_Fragment());
+                fr.add(R.id.holder,new Sign_Up_Fragment());
                 //fr.addToBackStack(null);
                 //fr.commit();
             }

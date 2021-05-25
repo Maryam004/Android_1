dea2d8ec28f8313a9a15e4ae373f5be1af95b25e
Maryam004 <mcsf19a039@pucit.edu.pk>
2021-05-24 22:04:24 -0700

    New Updation

--- Fragment_Practice/app/src/main/java/com/example/fragment_practice/MainActivity.java
+++ Fragment_Practice/app/src/main/java/com/example/fragment_practice/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.fragment_practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_SignIn,btn_SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_SignIn=findViewById(R.id.signIn);
+        btn_SignUp=findViewById(R.id.signUp);
+        btn_SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
